d299775cc74066f1cd8c7c9bd597f05e94d452a1
Matt <grafiszti@gmail.com>
2016-05-20 23:40:41 +0200

    loading map, some new assets

--- src/states/GameState.js
+++ src/states/GameState.js
@@ -3,17 +3,20 @@ import Grid from 'objects/Grid';
 import Player from 'objects/Player';
 
 class GameState extends Phaser.State {
-    create() {
+    preload(){
+		this.game.load.tilemap('myTileMap', '/assets/level11.json', null, Phaser.Tilemap.TILED_JSON);		
+		this.game.load.image('myTiles', '/assets/tileset.png');
+	}
+	
+	create() {
 		this.player1 = new Player();
 		let center = { x: this.game.world.centerX, y: this.game.world.centerY }
-		//let text = new RainbowText(this.game, center.x, center.y, "- phaser -\nwith a sprinkle of\nES6 dust!");
-		this.game.load.json('level', '../../static/levels/level1.json');
-		var levelJson = this.game.cache.getJSON('level');
 		
-		this.game.load.tilemap('tiles', '../../assets/level1.json', null, Phaser.Tilemap.TILED_JSON);
-        this.game.load.image('tiles', '/images/terrain_atlas.png');
-		
-		var text = this.game.add.text(100, 100, "Current Phaser version: ", { fill: '#ffffff' });	
+		map = this.game.add.tilemap('myTileMap');
+        map.addTilesetImage('tiles', 'myTiles');
+        layer = map.createLayer('layer');
+        layer.resizeWorld();
+        layer.wrap = true;
 	}
 }
 
