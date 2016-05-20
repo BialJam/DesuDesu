7f481979e56cafca28a0dc1dd8bc99956de1fd4c
konto <konto@users.noreply.github.com>
2016-05-20 21:20:37 +0200

    player something something

--- src/states/GameState.js
+++ src/states/GameState.js
@@ -1,15 +1,15 @@
 /// <reference path="../../node_modules/phaser/typescript/phaser.d.ts" />
-import RainbowText from 'objects/RainbowText';
+
 import Grid from 'objects/Grid';
 import Player from 'objects/Player';
 
 class GameState extends Phaser.State {
     create() {
-		this.player1 = new Player();
+		this.player1 = new Player(this.game);
 		let center = { x: this.game.world.centerX, y: this.game.world.centerY }
 		//let text = new RainbowText(this.game, center.x, center.y, "- phaser -\nwith a sprinkle of\nES6 dust!");
-		game.load.json('level', '../../static/levels/level1.json');
-		var levelJson = game.cache.getJSON('level');
+		this.game.load.json('level', '../../static/levels/level1.json');
+		var levelJson = this.game.cache.getJSON('level');
 		
 		var w = levelJson.w;
 		var h = levelJson.h;
