cd5522c8180796a6ebf32e0358220da7a93c6330
Matt <grafiszti@gmail.com>
2016-05-20 20:44:46 +0200

    new assets

---
+++ src/objects/Grid.js
@@ -0,0 +1,8 @@
+/// <reference path="../../node_modules/phaser/typescript/phaser.d.ts" />
+class Grid extends Phaser.Stage {
+	constructor(game, w, h) {
+		super(game);
+	}
+}
+
+export default Grid;
--- src/states/GameState.js
+++ src/states/GameState.js
@@ -1,11 +1,21 @@
+/// <reference path="../../node_modules/phaser/typescript/phaser.d.ts" />
 import RainbowText from 'objects/RainbowText';
+import Grid from 'objects/Grid';
 
 class GameState extends Phaser.State {
 
 	create() {
 		let center = { x: this.game.world.centerX, y: this.game.world.centerY }
-		let text = new RainbowText(this.game, center.x, center.y, "- phaser -\nwith a sprinkle of\nES6 dust!");
-		text.anchor.set(0.5);
+		//let text = new RainbowText(this.game, center.x, center.y, "- phaser -\nwith a sprinkle of\nES6 dust!");
+		game.load.json('level', '../../static/levels/level1.json');
+		var levelJson = game.cache.getJSON('level');
+		
+		var w = levelJson.w;
+		var h = levelJson.h;
+		var max = Math.max(w, h);
+		
+		var text = game.add.text(100, 100, "Current Phaser version: " + w + " " + h, { fill: '#ffffff' });
+		
 	}
 
 }
