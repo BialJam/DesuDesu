cdfe4419e6fd23046e0ce22c0803763ca38765d8
Matt <grafiszti@gmail.com>
2016-05-20 21:19:05 +0200

    fix

--- src/states/GameState.js
+++ src/states/GameState.js
@@ -1,5 +1,4 @@
 /// <reference path="../../node_modules/phaser/typescript/phaser.d.ts" />
-import RainbowText from 'objects/RainbowText';
 import Grid from 'objects/Grid';
 import Player from 'objects/Player';
 
@@ -8,14 +7,13 @@ class GameState extends Phaser.State {
 		this.player1 = new Player();
 		let center = { x: this.game.world.centerX, y: this.game.world.centerY }
 		//let text = new RainbowText(this.game, center.x, center.y, "- phaser -\nwith a sprinkle of\nES6 dust!");
-		game.load.json('level', '../../static/levels/level1.json');
-		var levelJson = game.cache.getJSON('level');
+		this.game.load.json('level', '../../static/levels/level1.json');
+		var levelJson = this.game.cache.getJSON('level');
 		
-		var w = levelJson.w;
-		var h = levelJson.h;
-		var max = Math.max(w, h);
+		this.game.load.tilemap('tiles', '../../assets/level1.json', null, Phaser.Tilemap.TILED_JSON);
+        this.game.load.image('tiles', '/images/terrain_atlas.png');
 		
-		var text = game.add.text(100, 100, "Current Phaser version: " + w + " " + h, { fill: '#ffffff' });	
+		var text = this.game.add.text(100, 100, "Current Phaser version: ", { fill: '#ffffff' });	
 	}
 }
 
