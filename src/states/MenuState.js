c34d4af2b7531827708dcbe371240f9525fcd46d
Wojciech Jaworowski <w.jaworowski92@gmail.com>
2016-05-21 11:08:06 +0200

    Loading

--- src/states/MenuState.js
+++ src/states/MenuState.js
@@ -22,7 +22,6 @@ class MenuState extends Phaser.State {
 
 function startOnClick() {
     console.log('DUPA1');
-    this.game.state.start('GameState');
     this.game.state.start('LoadingScreen');
 }
 
