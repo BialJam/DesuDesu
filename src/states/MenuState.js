62e7f9bdf5e4c98f3afb803edd9455e28cf1445d
konto <konto@users.noreply.github.com>
2016-05-21 20:44:22 +0200

    u

--- src/states/MenuState.js
+++ src/states/MenuState.js
@@ -5,10 +5,8 @@ import Player, {PlayerInfo} from 'objects/Player';
 
 class MenuState extends Phaser.State {
     create() {
-        this.game.add.tileSprite(0, 0, 960, 640, 'background-menu');
+        // this.game.add.tileSprite(0, 0, 960, 640, 'background-menu');
         let center = { x: this.game.world.centerX, y: this.game.world.centerY };
-        let startButton = new MyButton(this.game, 356, center.y, 'startGameSpritesheet', startOnClick, this, 0, 0, 1);
-
         this.playersColors = ['0x12fe00', '0xfff859', '0x0decfe', '0xfe544f'];
         this.buttons = ['any', 'up', 'down', 'left', 'right', 'action'];
         this.isActive = [false, false, false, false];
@@ -21,6 +19,14 @@ class MenuState extends Phaser.State {
         this.playerText = {};
         this.playerButtonId = {};
         this.playerButtons = [];
+        
+        for (let i = 0; i < 4; ++i) {
+            let color = this.playersColors[i];
+            this.playerText[i] = this.game.add.bitmapText(128, 250 + 16 + (i * 70), 'font', 'player' + (i+1) + ' - press any button to join', 32);
+            this.playerText[i].tint = color;
+            this.playerButtons[i] = this.add.sprite(32, 250 + (i * 70), 'colorButtons', 2);
+            this.playerButtons[i].tint = color;
+        }
     }
 
     update() {
@@ -36,29 +42,27 @@ class MenuState extends Phaser.State {
             let pad = this.game.input.gamepad['pad' + (id + 1)];
             let color = this.playersColors[this.playerNums];
             let padMap = {};
-            this.playerNums++;
-            this.playerz[id] = new PlayerInfo(pad, color, this.playerNums, padMap);
+            let pId = this.playerNums;
+            this.playerz[id] = new PlayerInfo(pad, color, this.playerNums + 1, padMap);
             this.playerButtonId[id] = 0;
-            this.playerText[id] = this.game.add.bitmapText(128, 250 + ((this.playerNums - 1) * 32), 'font', 'player' + this.playerNums, 32);
-            this.playerText[id].tint = color;
+            this.playerNums++;
             pad.addCallbacks(this, {
                 onDown: x => {
                     if (this.playerButtonId[id] < this.buttons.length) {
                         let a = this.buttons[this.playerButtonId[id]];
                         padMap[a] = x;
-                        console.log(x);
-                        console.log(padMap);
                         this.playerButtonId[id]++;
                         if (this.playerButtonId[id] + 1 > this.buttons.length) {
-                            this.playerText[id].text = "press 'action' to begin!";
+                            this.playerText[pId].text = "press 'action' to begin!";
                         }
                         else {
-                            this.playerText[id].text = this.buttons[this.playerButtonId[id]];
+                            this.playerText[pId].text = this.buttons[this.playerButtonId[id]];
                         }
                     }
                     else if (x === padMap['action']) {
-                        this.playerText[id].text = "ready!";
-                        this.isActive[id] = true;
+                        this.playerText[pId].text = "ready!";
+                        this.isActive[pId] = true;
+                        this.playerButtons[pId].frame = 1;
                     }
                 }
             });
