c438cc790092c3fce5b53bd06a22d9cf2ed79f7d
Wojciech Jaworowski <w.jaworowski92@gmail.com>
2016-05-21 17:42:40 +0200

    MenuState.js

--- src/states/MenuState.js
+++ src/states/MenuState.js
@@ -11,6 +11,7 @@ class MenuState extends Phaser.State {
         startButton.addText('Rozpocznij grę', 24);
 
         this.playersColors = ['0xff0000', '0x00ff00', '0x0000ff', '0xff00ff'];
+        this.isActive = [false, false, false, false];
         this.game.input.gamepad.start();
         this.game.input.gamepad.callbackContext = this;
         this.game.input.gamepad.onConnectCallback = this.addPlayer;
@@ -29,17 +30,22 @@ class MenuState extends Phaser.State {
     }
 
     update() {
-        if (this.game.input.gamepad.supported && this.game.input.gamepad.active && this.game.input.gamepad['pad1'] && this.game.input.gamepad['pad1'].isDown(Phaser.Gamepad.BUTTON_1) || this.game.input.gamepad['pad1'].isDown(Phaser.Gamepad.BUTTON_0) || this.game.input.gamepad['pad1'].isDown(Phaser.Gamepad.BUTTON_2) || this.game.input.gamepad['pad1'].isDown(Phaser.Gamepad.BUTTON_3) || this.game.input.gamepad['pad1'].isDown(Phaser.Gamepad.XBOX360_A) || this.game.input.gamepad['pad1'].isDown(Phaser.Gamepad.XBOX360_B) || this.game.input.gamepad['pad1'].isDown(Phaser.Gamepad.XBOX360_X) || this.game.input.gamepad['pad1'].isDown(Phaser.Gamepad.XBOX360_Y) || this.game.input.gamepad['pad1'].isDown(Phaser.Gamepad.PS3XC_SQUARE) || this.game.input.gamepad['pad1'].isDown(Phaser.Gamepad.PS3XC_CIRCLE) || this.game.input.gamepad['pad1'].isDown(Phaser.Gamepad.PS3XC_TRIANGLE) || this.game.input.gamepad['pad1'].isDown(Phaser.Gamepad.PS3XC_X)) {
+        if (this.isActive[2] === true && this.game.input.gamepad.supported && this.game.input.gamepad.active && this.game.input.gamepad['pad1'] && this.game.input.gamepad['pad1'].isDown(Phaser.Gamepad.BUTTON_1) || this.game.input.gamepad['pad1'].isDown(Phaser.Gamepad.BUTTON_0) || this.game.input.gamepad['pad1'].isDown(Phaser.Gamepad.BUTTON_2) || this.game.input.gamepad['pad1'].isDown(Phaser.Gamepad.BUTTON_3) || this.game.input.gamepad['pad1'].isDown(Phaser.Gamepad.XBOX360_A) || this.game.input.gamepad['pad1'].isDown(Phaser.Gamepad.XBOX360_B) || this.game.input.gamepad['pad1'].isDown(Phaser.Gamepad.XBOX360_X) || this.game.input.gamepad['pad1'].isDown(Phaser.Gamepad.XBOX360_Y) || this.game.input.gamepad['pad1'].isDown(Phaser.Gamepad.PS3XC_SQUARE) || this.game.input.gamepad['pad1'].isDown(Phaser.Gamepad.PS3XC_CIRCLE) || this.game.input.gamepad['pad1'].isDown(Phaser.Gamepad.PS3XC_TRIANGLE) || this.game.input.gamepad['pad1'].isDown(Phaser.Gamepad.PS3XC_X)) {
             console.log('DUPA3');
             this.game.state.start('GameState');
+        }
+        for (let id = 0; id < this.game.input.gamepad.padsConnected; id++) {
+            if (this.isActive[id] === false && this.game.input.gamepad.supported && this.game.input.gamepad.active && this.game.input.gamepad['pad' + (id + 1)] && this.game.input.gamepad['pad' + (id + 1)].isDown(Phaser.Gamepad.BUTTON_1) || this.game.input.gamepad['pad' + (id + 1)].isDown(Phaser.Gamepad.BUTTON_0) || this.game.input.gamepad['pad' + (id + 1)].isDown(Phaser.Gamepad.BUTTON_2) || this.game.input.gamepad['pad' + (id + 1)].isDown(Phaser.Gamepad.BUTTON_3) || this.game.input.gamepad['pad' + (id + 1)].isDown(Phaser.Gamepad.XBOX360_A) || this.game.input.gamepad['pad' + (id + 1)].isDown(Phaser.Gamepad.XBOX360_B) || this.game.input.gamepad['pad' + (id + 1)].isDown(Phaser.Gamepad.XBOX360_X) || this.game.input.gamepad['pad' + (id + 1)].isDown(Phaser.Gamepad.XBOX360_Y) || this.game.input.gamepad['pad' + (id + 1)].isDown(Phaser.Gamepad.PS3XC_SQUARE) || this.game.input.gamepad['pad' + (id + 1)].isDown(Phaser.Gamepad.PS3XC_CIRCLE) || this.game.input.gamepad['pad' + (id + 1)].isDown(Phaser.Gamepad.PS3XC_TRIANGLE) || this.game.input.gamepad['pad' + (id + 1)].isDown(Phaser.Gamepad.PS3XC_X)) {
+                console.log('pad ' + id + ' active');
+                let pPad = this.game.input.gamepad['pad' + (id + 1)];
+                let p = new PlayerInfo(pPad, this.playersColors[id], id);
+                this.game.players.push(p);
+            }
         }
     }
 
     addPlayer(id) {
         console.log("pad connected ", id);
-        let pPad = this.game.input.gamepad['pad' + (id + 1)];
-        let p = new PlayerInfo(pPad, this.playersColors[id], id);
-        this.game.players.push(p);
     }
 }
 
