85122e0b1b09fe0a1fd5685086c96ce497cc9082
Matt <grafiszti@gmail.com>
2016-05-21 10:36:13 +0200

    tileset readingworks

--- src/states/GameState.js
+++ src/states/GameState.js
@@ -3,25 +3,23 @@ import Grid from 'objects/Grid';
 import Player from 'objects/Player';
 
 class GameState extends Phaser.State {
-    preload(){
+	preload () {
 		this.game.load.tilemap('myTileMap', 'assets/level11.json', null, Phaser.Tilemap.TILED_JSON);		
-		this.game.load.image('myTiles', 'assets/tileset.png');
+		this.game.load.image('tiles', 'assets/tileset.png');
+		
+		this.load.spritesheet('cursor', 'assets/cursor.png', 16, 16, 4);		
 	}
-
-    create() {
+	
+	create() {
 		let player1 = new Player(this.game);
 		player1.create();
 		let center = { x: this.game.world.centerX, y: this.game.world.centerY }
 		
 		var map = this.game.add.tilemap('myTileMap');
-        map.addTilesetImage('tiles', 'myTiles');
-        var layer = map.createLayer('layer');
+        map.addTilesetImage('tileset', 'tiles');
+        var layer = map.createLayer('tiles');
         layer.resizeWorld();
         layer.wrap = true;
-	}
-	
-	preload () {
-		this.load.spritesheet('cursor', 'assets/cursor.png', 16, 16, 4);
 	}
 }
 
